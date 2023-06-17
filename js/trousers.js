3ebdce650e7cd7dfb649d80b4e0208645fe68d78
crongle <gasmazk73@gmail.com>
2023-06-17 12:50:04 +0100

    Add extension basics

---
+++ js/trousers.js
@@ -0,0 +1,23 @@
+console.log('Loaded.... BEEP');
+
+
+let mainShortsButton = document.querySelector('.ytd-mini-guide-renderer');
+
+
+console.log('BEEEEEEP');
+
+console.log(mainShortsButton);
+
+if ( mainShortsButton ) {
+    mainShortsButton.remove();
+}
+
+console.log('BEEEEEEPZ');
+
+
+
+
+  
+  
+  
+  
